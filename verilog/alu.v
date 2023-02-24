4c084e4b5e81f15f681a8ea79861c8d411ac641c
Jules PERRIN <jules.perrin@epfl.ch>
2023-02-24 11:10:15 +0100

    Added simulation file and reformated code

--- verilog/alu.v
+++ verilog/alu.v
@@ -1,25 +1,25 @@
-`include "parameters.vh"
-
 module alu (
-    input [31:0] a,b,
+    input reg [31:0] a,b,
     input [3:0] op_code,
-    output result
+    output reg [31:0] result
 );
+
+`include "parameters.vh"
 
 always @ (a,b, op_code) begin
     case (op_code)
-        ALU_ADD : result = a + b;
-        ALU_SUB : result = a - b;
+        ALU_ADD : result = $unsigned(a) + $unsigned(b);
+        ALU_SUB : result = $unsigned(a) - $unsigned(b);
         ALU_XOR : result = a ^ b;
         ALU_OR : result = a | b;
         ALU_AND : result = a & b;
         ALU_SSL : result = a << b;
         ALU_SRL : result = a >> b;
         ALU_SRA : result = a >>> b;
-        ALU_SLT : result = a < b;
-        ALU_SLTU : result = a < b;
+        ALU_SLT : result = $signed(a) < $signed(b);
+        ALU_SLTU : result = $unsigned(a) < $unsigned(b);
         ALU_NOP : result = a; 
-        default: 
+        default:
             result = 0;
     endcase
 end
