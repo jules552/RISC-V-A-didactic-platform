f4028d0b921e3e2b84cd3b2e6a1d02efe00b4b11
Jules PERRIN <jules.perrin@epfl.ch>
2023-03-23 23:38:08 +0100

    Fixed it, but don't like it :(

--- verilog/pc.v
+++ verilog/pc.v
@@ -7,7 +7,7 @@ module pc (
 // We change the PC on the rising edge of the clock
 always @ (posedge clk or negedge reset_n) begin
     if (!reset_n) begin
-        pc <= 0;
+        pc <= -4;
     end
     else begin
         pc <= new_pc;
