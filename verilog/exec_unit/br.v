e5df419680f444641852251b58ba57db66df2963
Jules PERRIN <jules.perrin@epfl.ch>
2023-03-24 18:33:00 +0100

    Better br too

--- verilog/exec_unit/br.v
+++ verilog/exec_unit/br.v
@@ -1,15 +1,16 @@
 module br (
-    input reg [31:0] pc,
-    input reg [31:0] imm,
-    input reg br_sig,
-    input reg [31:0] alu_out,
-    input reg [2:0] br_op,
+    input wire [31:0] pc,
+    input wire [31:0] imm,
+    input wire br_sig,
+    input wire [31:0] alu_out,
+    input wire [2:0] br_op,
     output reg [31:0] new_pc,
     output reg [31:0] pc_plus4
 );
 
     `include "../parameters.vh"
 
+    wire signed [31:0] alu_out_signed;
     always @ (*) begin
         pc_plus4 = pc + 4;
 
@@ -17,10 +18,10 @@ module br (
             case (br_op)
                 BR_BEQ : new_pc = alu_out == 0 ? pc + imm : pc + 4;
                 BR_BNE : new_pc = alu_out != 0 ? pc + imm : pc + 4;
-                BR_BLT : new_pc = $signed(alu_out) < 0 ? pc + imm : pc + 4;
-                BR_BLTU : new_pc = $unsigned(alu_out) < 0 ? pc + imm : pc + 4;
-                BR_BGE : new_pc = $signed(alu_out) >= 0 ? pc + imm : pc + 4;
-                BR_BGEU : new_pc = $unsigned(alu_out) >= 0 ? pc + imm : pc + 4;
+                BR_BLT : new_pc = alu_out_signed < 0 ? pc + imm : pc + 4;
+                BR_BLTU : new_pc = alu_out < 0 ? pc + imm : pc + 4;
+                BR_BGE : new_pc = alu_out_signed >= 0 ? pc + imm : pc + 4;
+                BR_BGEU : new_pc = alu_out >= 0 ? pc + imm : pc + 4;
                 BR_JALR : new_pc = alu_out & ~1;
                 BR_JAL : new_pc = alu_out;
                 default: new_pc = pc + 4;
