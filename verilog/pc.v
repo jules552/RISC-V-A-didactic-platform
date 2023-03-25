83e2c6ecb6ef040eedb9b3c8800debf26b2819af
Jules PERRIN <jules.perrin@epfl.ch>
2023-03-25 16:06:21 +0100

    Better logic in PC

--- verilog/pc.v
+++ verilog/pc.v
@@ -1,16 +1,20 @@
 module pc (
     input wire reset_n, clk,
     input wire [31:0] new_pc,
-    output reg [31:0] pc
+    output wire [31:0] pc
 );
+
+reg [31:0] pc_reg;
+
+assign pc = pc_reg;
 
 // We change the PC on the rising edge of the clock
 always @ (posedge clk or negedge reset_n) begin
     if (!reset_n) begin
-        pc <= -4;
+        pc_reg <= -4;
     end
     else begin
-        pc <= new_pc;
+        pc_reg <= new_pc;
     end
 end
 
