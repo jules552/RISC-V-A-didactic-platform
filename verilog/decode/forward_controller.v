1a44dccaaac2d826968c4e405c210949e1d35581
Jules PERRIN <jules.perrin@epfl.ch>
2023-05-15 13:05:43 +0200

    Revert "Maybe better by taking values in registers ?"
    This reverts commit 64d1a676aac8c252333f82760dadc2689ad5fe40.

--- verilog/decode/forward_controller.v
+++ verilog/decode/forward_controller.v
@@ -3,13 +3,17 @@ module forward_controller (
     input wire [31:0] reg_rs1_i,
     input wire [4:0] reg_addr2_i,
     input wire [31:0] reg_rs2_i,
-
+    input wire [4:0] id_ex_reg_wr_addr_i,
+    input wire id_ex_reg_wr_sig_i,
+    input wire [1:0] id_ex_data_dest_i,
+    input wire [31:0] id_ex_alu_result_i,
+    input wire [31:0] id_ex_pc_plus4_i,
     input wire [4:0] ex_mem_reg_wr_addr_i,
     input wire ex_mem_reg_wr_sig_i,
     input wire [1:0] ex_mem_data_dest_i,
     input wire [31:0] ex_mem_alu_result_i,
     input wire [31:0] ex_mem_pc_plus4_i,
-    
+    input wire [31:0] ex_mem_mem_rd_data_i,
     input wire [4:0] mem_wb_reg_wr_addr_i,
     input wire mem_wb_reg_wr_sig_i,
     input wire [1:0] mem_wb_data_dest_i,
@@ -31,18 +35,35 @@ module forward_controller (
         rs1_forward = reg_rs1_i;
         rs2_forward = reg_rs2_i;
 
-        // If there is a data hazard, forward the data from the previous stage
+        if (id_ex_reg_wr_sig_i && id_ex_reg_wr_addr_i != 5'd0 && id_ex_reg_wr_addr_i == reg_addr1_i) begin
+            if (id_ex_data_dest_i == ALU) begin
+                rs1_forward = id_ex_alu_result_i;
+            end else if (id_ex_data_dest_i == PC) begin
+                rs1_forward = id_ex_pc_plus4_i;
+            end
+        end else if (id_ex_reg_wr_sig_i && id_ex_reg_wr_addr_i != 5'd0 && id_ex_reg_wr_addr_i == reg_addr2_i) begin
+            if (id_ex_data_dest_i == ALU) begin
+                rs2_forward = id_ex_alu_result_i;
+            end else if (id_ex_data_dest_i == PC) begin
+                rs2_forward = id_ex_pc_plus4_i;
+            end
+        end
+
         if (ex_mem_reg_wr_sig_i && ex_mem_reg_wr_addr_i != 5'd0 && ex_mem_reg_wr_addr_i == reg_addr1_i) begin
             if (ex_mem_data_dest_i == ALU) begin
                 rs1_forward = ex_mem_alu_result_i;
             end else if (ex_mem_data_dest_i == PC) begin
                 rs1_forward = ex_mem_pc_plus4_i;
+            end else if (ex_mem_data_dest_i == MEM) begin
+                rs1_forward = ex_mem_mem_rd_data_i;
             end
         end else if (ex_mem_reg_wr_sig_i && ex_mem_reg_wr_addr_i != 5'd0 && ex_mem_reg_wr_addr_i == reg_addr2_i) begin
             if (ex_mem_data_dest_i == ALU) begin
                 rs2_forward = ex_mem_alu_result_i;
             end else if (ex_mem_data_dest_i == PC) begin
                 rs2_forward = ex_mem_pc_plus4_i;
+            end else if (ex_mem_data_dest_i == MEM) begin
+                rs2_forward = ex_mem_mem_rd_data_i;
             end
         end
 
