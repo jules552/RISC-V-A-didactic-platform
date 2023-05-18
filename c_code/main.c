82cd01c38af4fa54f2079bfd83e9c33622678566
Jules Perrin <jules.perrin@epfl.ch>
2023-05-18 14:25:40 +0200

    Easier to see the result

--- c_code/main.c
+++ c_code/main.c
@@ -1,13 +1,11 @@
 void _start() {
-    asm volatile("call main");    
-    while (1);
-}
-
-int main() {
     int sum = 0;
     for(int i = 1; i < 10; ++i) {
         sum += i;
     }
 
-    return sum;
+    asm volatile ("mv x29, %0" :: "r"(sum));
+    while(1);
 }
+
+
