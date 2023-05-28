09468b076881c05328d1e733bba960d646d8928e
Jules PERRIN <jules.perrin@epfl.ch>
2023-05-29 00:34:28 +0200

    Added tests for register_file module

--- tb/alu_tb.v
+++ tb/alu_tb.v
@@ -13,6 +13,8 @@ module alu_tb;
     integer i;
     localparam NB_RANDOM_TESTS = 1000;
 
+    integer num_failures;
+
     alu alu_inst (
         .a(a),
         .b(b),
@@ -21,105 +23,88 @@ module alu_tb;
     );
 
     initial begin
+        num_failures = 0; // Reset failure counter
+
         // Test ALU_ADD
         alu_op = ALU_ADD;
-
-        // Test Basic Addition
-        // Run 1000 tests
-        for(i=0; i<NB_RANDOM_TESTS; i=i+1)
-        begin
+        for(i=0; i<NB_RANDOM_TESTS; i=i+1) begin
             a = $random;
             b = $random;
             #10;
             if (result != a + b) begin
-                $display("ALU_ADD: FAIL");
-                $display("ALU_ADD: %d + %d = %d", a, b, result);
+                num_failures = num_failures + 1;
+                $display("ALU_ADD: %d + %d != %d", a, b, result);
             end
         end 
 
-        // Test Corner Case Overflow
         a = 1;
         b = -1; // = 0xFFFFFFFF
         #10;
         if (result != 0) begin
-            $display("ALU_ADD: FAIL");
+            num_failures = num_failures + 1;
+            $display("ALU_ADD: 1 + (-1) != 0");
         end 
-        $display("ALU_ADD: PASS");
 
         // Test ALU_SUB
         alu_op = ALU_SUB;
-        
-        // Test Basic Subtraction
-        // Run 1000 tests
-        for(i=0; i<NB_RANDOM_TESTS; i=i+1)
-        begin
+        for(i=0; i<NB_RANDOM_TESTS; i=i+1) begin
             a = $random;
             b = $random;
             #10;
             if (result != a - b) begin
-                $display("ALU_SUB: FAIL");
-                $display("ALU_SUB: %d - %d = %d", a, b, result);
+                num_failures = num_failures + 1;
+                $display("ALU_SUB: %d - %d != %d", a, b, result);
             end
         end
 
-        // Test Corner Case Underflow
         a = 0;
         b = 1;
         #10;
         if (result != -1) begin
-            $display("ALU_SUB: FAIL");
+            num_failures = num_failures + 1;
+            $display("ALU_SUB: 0 - 1 != -1");
         end
-        $display("ALU_SUB: PASS");
 
         // Test ALU_XOR
         alu_op = ALU_XOR;
-
-        // Test Basic XOR
-        // Run 1000 tests
         for(i=0; i<NB_RANDOM_TESTS; i=i+1) begin
             a = $random;
             b = $random;
             #10;
             if (result != (a ^ b)) begin
-                $display("ALU_XOR: FAIL");
-                $display("ALU_XOR: %d ^ %d = %d", a, b, result);
+                num_failures = num_failures + 1;
+                $display("ALU_XOR: %d ^ %d != %d", a, b, result);
             end
         end
-        $display("ALU_XOR: PASS");
 
         // Test ALU_OR
         alu_op = ALU_OR;
-
-        // Test Basic OR
-        // Run 1000 tests
         for(i=0; i<NB_RANDOM_TESTS; i=i+1) begin
             a = $random;
             b = $random;
             #10;
             if (result != (a | b)) begin
-                $display("ALU_OR: FAIL");
-                $display("ALU_OR: %d | %d = %d", a, b, result);
+                num_failures = num_failures + 1;
+                $display("ALU_OR: %d | %d != %d", a, b, result);
             end
         end
-        $display("ALU_OR: PASS");
 
         // Test ALU_AND
         alu_op = ALU_AND;
-
-        // Test Basic AND
-        // Run 1000 tests
         for(i=0; i<NB_RANDOM_TESTS; i=i+1) begin
             a = $random;
             b = $random;
             #10;
             if (result != (a & b)) begin
-                $display("ALU_AND: FAIL");
-                $display("ALU_AND: %d & %d = %d", a, b, result);
+                num_failures = num_failures + 1;
+                $display("ALU_AND: %d & %d != %d", a, b, result);
             end
         end
-        $display("ALU_AND: PASS");
+
+        if (num_failures == 0) begin
+            $display("ALU: ALL TESTS PASSED");
+        end
 
         $finish;
     end
-
 endmodule
---
+++ tb/register_file_tb.v
@@ -0,0 +1,92 @@
+`timescale 1ns / 1ps
+
+module test_register_file;
+
+    reg clk;
+    reg reset_n;
+    reg [4:0] rs1_addr_i;
+    reg [4:0] rs2_addr_i;
+    reg [31:0] wr_data_i;
+    reg [4:0] wr_addr_i;
+    reg wr_enable_i;
+
+    wire [31:0] rs1_o;
+    wire [31:0] rs2_o;
+
+    reg [31:0] expected_values[0:31]; // Store expected values for verification later
+
+    integer i;
+    integer num_failures; // To keep track of number of test failures
+
+    // Instantiate the module
+    register_file rf (
+        .clk(clk),
+        .reset_n(reset_n),
+        .rs1_addr_i(rs1_addr_i),
+        .rs2_addr_i(rs2_addr_i),
+        .wr_data_i(wr_data_i),
+        .wr_addr_i(wr_addr_i),
+        .wr_enable_i(wr_enable_i),
+        .rs1_o(rs1_o),
+        .rs2_o(rs2_o)
+    );
+
+    // Create a clock with period 20ns
+    always begin
+        #10 clk = ~clk;
+    end
+
+    // Test procedure
+    initial begin
+        // Reset
+        clk = 0; reset_n = 0;
+        rs1_addr_i = 0; rs2_addr_i = 0;
+        wr_data_i = 0; wr_addr_i = 0;
+        wr_enable_i = 0;
+        num_failures = 0; // Reset failure counter
+
+        #15; // Wait for some time
+        reset_n = 1;
+
+        // Write random values to each register
+        for (i = 0; i < 32; i = i+1) begin
+            #10; // Wait for one clock cycle
+
+            wr_data_i = $random; 
+            wr_addr_i = i;
+            wr_enable_i = 1;
+
+            if (i == 0) begin
+                expected_values[i] = 0; // Register 0 is always 0
+            end else begin
+                expected_values[i] = wr_data_i;
+            end
+
+            #10; // Wait for one clock cycle
+
+            wr_enable_i = 0; 
+        end
+
+        // Read back and verify each register
+        for (i = 0; i < 32; i = i+1) begin
+            #10; // Wait for one clock cycle
+
+            rs1_addr_i = i;
+
+            #10; // Wait for one clock cycle
+
+            if (rs1_o != expected_values[i]) begin
+                num_failures = num_failures + 1;
+                $display("REGISTER %d: FAILED", i);
+                $display("Expected: %h, Got: %h", expected_values[i], rs1_o);
+            end
+        end
+
+        if (num_failures == 0) begin
+            $display("REGISTER FILE: ALL TESTS PASSED");
+        end
+
+        // End the test
+        $finish;
+    end
+endmodule
